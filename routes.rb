acdac51a4ee96eb8c7b294bc8f8fba806f4a090e
arao99 <yusuque.swallows@gmail.com>
2021-08-10 00:51:32 +0900

    [WIP]create top page

--- routes.rb
+++ routes.rb
@@ -1,5 +1,38 @@
 require 'sinatra'
+require './database'
 
 get '/' do
-  'Hello world!'
+  erb :index
+end
+
+get '/post' do
+  'post!'
+end
+
+post '/post_record' do
+  'post_record!'
+end
+
+get '/browse' do
+  'browse!'
+end
+
+get '/readme' do
+  'readme!'
+end
+
+get '/history' do
+  'history!'
+end
+
+get '/todo' do
+  'todo!'
+end
+
+not_found do
+  '404!'
+end
+
+error do
+  'error!'
 end
